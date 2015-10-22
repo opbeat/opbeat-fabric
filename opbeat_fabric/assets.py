68dc7a04c757fe33d052bdef49952f28d6f40ec4
Ron Cohen <ron@opbeat.com>
2015-10-22 10:53:07 +0200

    Attempt at making sure local attributes are not transferred with assets.

--- opbeat_fabric/assets.py
+++ opbeat_fabric/assets.py
@@ -33,7 +33,7 @@ def sync_assets():
         )
         local("s3cmd --configure")
 
-    local("s3cmd sync {} s3://{}".format(env.asset_build_dir, env.asset_bucket))
+    local("s3cmd --no-preserve sync {} s3://{}".format(env.asset_build_dir, env.asset_bucket))
 
 
 @task
